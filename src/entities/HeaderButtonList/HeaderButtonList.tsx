7d8b53c21efe6bb3290d1e0a407e02815c126cd5
DmitryRudinsky <guladmitriy1331@gmail.com>
2025-01-14 04:18:07 +0300

    rename Button List

---
+++ src/entities/HeaderButtonList/HeaderButtonList.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './HeaderButtonList.module.scss';
+import SystemButton from "../../shared/ui/SystemButton/SystemButton.tsx";
+import AddIcon from './assets/add.svg?react';
+import Flag from './assets/RU.svg?react';
+import Stats from  './assets/stats.svg?react';
+import Settings from  './assets/settings.svg?react';
+import Question from  './assets/question.svg?react'
+
+
+interface ButtonListProps {
+    isPlaying: boolean;
+    country: string;
+    setWindow: React.Dispatch<React.SetStateAction<string>>;
+    window: string;
+}
+
+const HeaderButtonList: React.FC<ButtonListProps> = ({isPlaying, country, setWindow, window}) => {
+    return (
+        <div className={styles.buttonList}>
+            <div className={styles.buttonList__left}>
+                <SystemButton isText={false} textContent={country} setWindow={setWindow} option={'dictionary'} window={window}>
+                    <Flag/>
+                </SystemButton>
+                <SystemButton isText={false} textContent={null} setWindow={setWindow} option={'add'} window={window}>
+                    <AddIcon/>
+                </SystemButton>
+                {isPlaying ?
+                    <SystemButton isText={true} textContent={null} setWindow={setWindow} option={'giveUp'} window={window}>
+                        Я сдаюсь
+                    </SystemButton>
+                    :
+                    <></>}
+            </div>
+            <div className={styles.buttonList__right}>
+                <SystemButton isText={false} textContent={null} setWindow={setWindow} option={'stats'} window={window}>
+                    <Stats/>
+                </SystemButton>
+                <SystemButton setWindow={setWindow} isText={false} textContent={null} option={'settings'} window={window}>
+                    <Settings/>
+                </SystemButton>
+                <SystemButton isText={false} textContent={null} setWindow={setWindow} option={'question'} window={window}>
+                    <Question/>
+                </SystemButton>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderButtonList;
